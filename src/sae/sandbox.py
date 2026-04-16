3448a1665080aa97f7796dd01c35955e40e2ee31
IParraMartin <82852463+IParraMartin@users.noreply.github.com>
2026-04-16 12:33:57 -0700

    Refactor sandbox script to use model outputs for feature activation analysis and improve code organization

--- src/sae/sandbox.py
+++ src/sae/sandbox.py
@@ -1,17 +1,29 @@
-from sae_lens import SAE
 import torch
+from sae_lens import SAE
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
+
+# Pretrained models here:
 # https://decoderesearch.github.io/SAELens/latest/pretrained_saes/
 
+HF_MODEL = "gpt2"
 RELEASE = "gpt2-small-res-jb"
 ID = "blocks.6.hook_resid_pre"
+HIDDEN_STATE_L = 6
 DEVICE = "cpu"
 
+text = "The house in the forest"
+
 sae = SAE.from_pretrained(RELEASE, ID, device=DEVICE)
+model = AutoModelForCausalLM.from_pretrained(HF_MODEL, cache_dir="cache")
+tokenizer = AutoTokenizer.from_pretrained(HF_MODEL, cache_dir="cache")
 
+inputs = tokenizer(text, return_tensors="pt")
+with torch.no_grad():
+    out = model(**inputs, output_hidden_states=True)
 
-activations = torch.randn(1, 5, 768, device="cpu")
-feature_acts = sae.encode(activations)
+acts = out.hidden_states[HIDDEN_STATE_L]
+feature_acts = sae.encode(acts)
 
 # Check which features are active
 active_features = (feature_acts > 0).sum(dim=-1)
@@ -20,4 +32,5 @@ print(f"Active feats (idx): {active_features}")
 print(f"Average L0: {active_features.float().mean().item()}")
 
 
-
+if __name__ == "__main__":
+    pass
